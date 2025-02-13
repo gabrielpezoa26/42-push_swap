a171e81da887e746518cc01474ec3f1fb0a26fe3
Gabriel Cesar Nunes Pezoa <gcesar-n@c1r3p4.42sp.org.br>
2025-02-13 14:28:40 -0300

    -

--- sort_big.sh
+++ sort_big.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "🔥 Running Full push_swap Test with Move Counting (100-500 numbers) 🔥"
+echo " ---Running Full push_swap Test with Move Counting (100-500 numbers)--- "
 
 ### 🚀 Function to Test and Count Moves ###
 test_case() {
